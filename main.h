d66b7eae9dd2236e60f14e84d7804f486960faec
weedii <abidiw293@gmail.com>
2022-11-09 10:11:58 +0100

    test

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print(const char *format, va_list args);
+
+#endif
---
+++ print.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print - print and return lengh
+ * @format: type of variable to be printed
+ * @args: arguments
+ * Return: the number of characters printed
+ */
+
+int print(const char *format, va_list args)
+{
+	int lengh = 0;
+	char *s, c;
+
+	switch (*format)
+	{
+	case 'c':
+		c = va_arg(args, int);
+		putchar(c);
+		lengh++;
+		break;
+	case 's':
+		s = va_arg(args, char *);
+		if (s == NULL)
+			s = "(null)";
+		while (*s != '\0')
+		{
+			putchar(*s++);
+			lengh++;
+		}
+		break;
+	case '%':
+		putchar('%');
+		lengh++;
+		break;
+	default:
+		putchar('%');
+		lengh++;
+		putchar(*format);
+		lengh++;
+		break;
+	}
+	return (lengh);
+}
