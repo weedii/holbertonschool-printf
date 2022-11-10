4056549fe5d8b62043cc8eb0f589e68ce87e8792
weedii <abidiw293@gmail.com>
2022-11-10 07:50:12 +0100

    get print function

--- get_print_function.c
+++ get_print_function.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
-/***/
+/**
+ * get_print_function - ontain the function that selects the correct
+ *		function to perform the operation asked by the user
+ * @c: format
+ * @args: arguments
+ * Return: pointer to the function that corresponds
+ */
 
 int get_print_function(const char c, va_list args)
 {
-    int i = 0;
+	int i = 0;
 
-    print_struct table[] = {
-        {'c', print_char},
-        {'s', print_string},
-        {'d', print_d},
-        {'i', print_i}};
+	print_struct table[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_d},
+		{'i', print_i}};
 
-    while (table[i].ch)
-    {
-        if (table[i].ch == c)
-            return (table[i].f(args));
-        i++;
-    }
-    return (0);
+	while (table[i].ch)
+	{
+		if (table[i].ch == c)
+			return (table[i].f(args));
+		i++;
+	}
+	return (0);
 }
