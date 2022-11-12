ef86894e503f9200b80064e39df601c5a59cad37
weedii <abidiw293@gmail.com>
2022-11-12 18:20:07 +0100

    conversion function

---
+++ convertion_int_function.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * convert - function that convert between bases
+ * @number: number to be convertd
+ * @base:   base to convert to
+ * Return:  the converted number
+ */
+
+int convert(int number, int base)
+{
+	if (number == 0 || base == 10)
+		return (number);
+	return ((number % base) + 10 * convert(number / base, base));
+}
